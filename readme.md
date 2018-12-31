ee653af2e193ed18b808d55b323200239bb2c2cc
theandychung <theandychung@gmail.com>
2018-12-31 02:34:25 -0800

    update setup.py update AuditoryModel polish the package

--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
+# Cython_IPEM
+## Auditory Model
+Please visit [HERE](https://github.com/IPEM/IPEMToolbox) to get the latest IPEM Toolbox
+## Build
+Currently Only the following version has been built:
+- Python2.7 64bit
 
+If the version of python you are using is not listed above, please build your own extension by the following steps:
+### Find proper compiler
+https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
+
+### Compile to build
 ```
 python setup.py build_ext --inplace --compiler=msvc
+```
+python2.7 can use msvc.
+
+Read More:
+https://docs.python.org/2/install/index.html#using-non-microsoft-compilers-on-windows
+
+## Install
+Install the built extension by the following command:
+
+```
 python setup.py install
 ```
-reference: https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
+Read More:
+https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
--- setup.py
+++ setup.py
@@ -1,6 +1,37 @@
 from setuptools import setup
 from setuptools.extension import Extension
+import os
 
+
+def list_all_src_files(foldername):
+    """
+    find all source files(.c) in 'foldername'
+    and list their directories
+    """
+    sourcefiles = ["mycodecpy.pyx"]
+    for r, d, f in os.walk(foldername):
+        for file in f:
+            if ".c" in file and ".cpp" not in file:
+                sourcefiles.append(os.path.join(r, file))
+    return sourcefiles
+
+
+def list_all_header_folders(folder):
+    """
+    find all folders containing header files(.h)
+    and list their directories
+    """
+    dirs = []
+    for r, d, f in os.walk(folder):
+        for file in f:
+            if ".h" in file:
+                if not os.path.join(r) in dirs:
+                    dirs.append(os.path.join(r))
+    return dirs
+
+
+# path of Auditory Model Folder
+AuditoryModel = 'AuditoryModel'
 
 setup(
     name="mycodecpy",
@@ -12,10 +43,17 @@ setup(
         'Cython',
     ],
     ext_modules=[
-     Extension("mycodecpy", ["mycodecpy.pyx", "cpu.c", "cpupitch.c",
-                          "decimation.c", "ecebank.c", "filterbank.c",
-                          "Hcmbank.c", "command.c", "filenames.c",
-                          "pario.c", "sigio.c", "IPEMAuditoryModel.c", "Audimod.c", "AudiProg.c"])
+     Extension("mycodecpy",
+               sources=list_all_src_files(AuditoryModel),
+               include_dirs=list_all_header_folders(AuditoryModel),
+               )
     ],
+    # required to run IPEM.py
+    install_requires=[
+        'brian',
+        'scipy',
+        'numpy',
+        'sympy',
+    ]
 )
-# brian, scipy, numpy
+# https://stackoverflow.com/questions/43163315/how-to-include-header-file-in-cython-correctly-setup-py
